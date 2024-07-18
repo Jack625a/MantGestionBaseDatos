46ddfd1cd478c44abdfff35361fa5feb666ace73
jesus121212121 <jesusgaray89787@gmail.com>
2024-07-18 11:32:59 -0400

    hola

--- insertarEstudiantes.php
+++ insertarEstudiantes.php
@@ -5,6 +5,7 @@
     $usuario='root';
     $contraseña='';
     $nombreBaseDatos='institutocatec';
+    //prueba
 
     //PASO2. CREAR Y ESTABLECER LA CONEXION
     $conexion=new mysqli($nombreServidor,$usuario,$contraseña,$nombreBaseDatos);
